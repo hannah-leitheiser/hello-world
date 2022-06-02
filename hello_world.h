6e62f13cf197896587463a57bed9161f745b3753
hannah-leitheiser <leitheiserhannah@runbox.com>
2022-06-02 18:55:36 -0500

    Update hello_world.h

--- hello_world.h
+++ hello_world.h
@@ -15,18 +15,22 @@ char* Help_Message =  "Usage: ./hello_world [OPTIONS]\n"
                       "\n"
                       " any arguments - Prints this message.\n"
                       " no arguments  - Prints Hello World.\n";
-/* Mutl-line string: Fisher, 2016 */
+/* [Mutl-line string: Fisher, 2016] */
+/* [Help message format: Torbjorn & Stallman, 2022, line 93-125. ] */
 
 int main(int argc, char *argv[]); /* [from WG14, 2018, p. 11] */
 
 /* Works Cited:
- * Borini, Stefano & Smith, Jed.  2009.  "What do .c and .h file extensions mean to C?"  Stack Overflow.  
- *    Retrieved from https://stackoverflow.com/questions/1695224/what-do-c-and-h-file-extensions-mean-to-c on 
- * Brian W. Kernighan & Dennis M. Ritchie.  1988.  The C Programming Language.  
- *     Second Edition.  Prentise Hall.  ISBN 0-13-110370-9. 
- * Fisher, Jim. (2016). How do I write a multi-line string literal in C?.  
- *     Retrieved from https://jameshfisher.com/2016/11/30/c-multiline-literal/ on 2022 June 03.
- * WG14. (2018).  Programming Languages -- C. 9899:202x (E).  ISO/IEC.
- *     Retrieved from https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
- *     on 2022 June 03.
+ *  Borini, Stefano & Smith, Jed.  2009.  "What do .c and .h file extensions mean to C?"  Stack Overflow.  
+ *     Retrieved from https://stackoverflow.com/questions/1695224/what-do-c-and-h-file-extensions-mean-to-c on 
+ *  Brian W. Kernighan & Dennis M. Ritchie.  1988.  The C Programming Language.  
+ *      Second Edition.  Prentise Hall.  ISBN 0-13-110370-9. 
+ *  Fisher, Jim. (2016). How do I write a multi-line string literal in C?.  
+ *      Retrieved from https://jameshfisher.com/2016/11/30/c-multiline-literal/ on 2022 June 03.
+ *  Granlund, Torbjorn & Stallman, Richard M.  2022.  cat.c.  GNU core utilities.  
+ *      Retrieved from https://github.com/coreutils/coreutils/blob/master/src/cat.c on 2022 June 03.
+ *  WG14. (2018).  Programming Languages -- C. 9899:202x (E).  ISO/IEC.
+ *      Retrieved from https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
+ *      on 2022 June 03.
+ *
  */
