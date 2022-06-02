d69c4e34a3f43804e3d0f47797bebcafdd57c1cf
hannah-leitheiser <leitheiserhannah@runbox.com>
2022-06-02 17:34:12 -0500

    Create hello_world.h

---
+++ hello_world.h
@@ -0,0 +1,22 @@
+/* Filename        : hello_world.h
+ * Type of File    : C Source Code, Header
+ * Date Created    : 2022JUN02
+ * Author          : Hannah Leitheiser
+ * Project Name    : Hello World
+ * Description     : Print "Hello World!" to stdout.
+ * Compile         : gcc hello_world.c -o hello_world
+ *  and Run        : ./hello_world
+ * [Comment Syntax: Kernighan & Ritchie, 1988, p. 9]
+ * [File Use: Stefano & Smith, 2009 ] */
+
+int main(int argc, char *argv[]); /* [from WG14, 2018, p. 11] */
+
+/* Works Cited:
+ * Borini, Stefano & Smith, Jed.  2009.  "What do .c and .h file extensions mean to C?"  Stack Overflow.  
+ *    Retrieved from https://stackoverflow.com/questions/1695224/what-do-c-and-h-file-extensions-mean-to-c on 
+ * Brian W. Kernighan & Dennis M. Ritchie.  1988.  The C Programming Language.  
+ *     Second Edition.  Prentise Hall.  ISBN 0-13-110370-9. 
+ *  WG14. (2018).  Programming Languages -- C. 9899:202x (E).  ISO/IEC.
+ *     Retrieved from https://www.open-std.org/jtc1/sc22/wg14/www/docs/n2310.pdf
+ *     on 2022 June 03.
+ */
